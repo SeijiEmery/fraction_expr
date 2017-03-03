5da55646f15d0ed2a7b78473140f62d4325e4faf
Seiji Emery <seijiemery@gmail.com>
2017-03-03 00:54:33 -0800

    build system + unit tests
    WIP

--- src/fraction.hpp
+++ src/fraction.hpp
@@ -16,7 +16,11 @@
     #define FRACTION_NO_STRING_PARSING 0
 #endif
 
-#include <numeric>      // std::gcd in Fraction
+#include <algorithm>
+#define gcd std::__gcd
+
+
+// #include <numeric>      // std::gcd in Fraction
 #include <ostream>      // std::ostream in Fraction
 #include <istream>      // std::istream in Fraction + FractionParser
 #include <string>       // std::string in Fraction + FractionParser
@@ -29,25 +33,28 @@
     #include <vector>       // std::vector in FractionParser
 #endif
 
+#include <iostream>
 
 struct Fraction {
     int         n;
-    unsigned    d;
+    int    d;
 
-    Fraction (decltype(n) n = 0, decltype(d) d = 1) : n(n), d(d) {
-        auto cf = std::gcd(n, d);
-        if (cf) {
-            n /= cf;
-            d /= cf;
+    Fraction (int n = 0, int d = 1) : n(n), d(d) {
+        if (d != 0) {
+            auto cf = gcd(abs(n), abs(d));
+            if (cf) {
+                std::cout << "Reducing " << n << "/" << d;
+                this->n /= cf;
+                this->d /= cf;
+                std::cout << " to " << this->n << "/" << this->d << '\n';
+            }
         }
     }
     // Default copy-ctor
     Fraction (const Fraction&) = default;
 
     // From-string ctor
-    Fraction (const std::string& s) : n(0), d(1) {
-        std::stringstream { s } >> *this;
-    }
+    Fraction (const std::string& s);
 
     auto& operator= (const Fraction& other) {
         n = other.n;
@@ -56,8 +63,8 @@ struct Fraction {
     }
 
     #define DEFN_CMP(op) \
-    friend bool operator cmp (const Fraction& a, const Fraction& b) {
-        return (a.n * b.d) op (b.n * a.d);
+    friend bool operator op (const Fraction& a, const Fraction& b) { \
+        return (a.n * b.d) op (b.n * a.d); \
     }
     DEFN_CMP(==)
     DEFN_CMP(!=)
@@ -74,10 +81,10 @@ struct Fraction {
         return { a.n * b.n, a.d * b.d };
     }
     friend Fraction operator- (const Fraction& a, const Fraction& b) {
-        return a + { -b.n, b.d };
+        return a + Fraction { -b.n, b.d };
     }
     friend Fraction operator/ (const Fraction& a, const Fraction& b) {
-        return a * { b.d, b.n };
+        return a * Fraction { b.d, b.n };
     }
 
     auto& operator += (const Fraction& other) { return *this = (*this + other); }
@@ -85,8 +92,8 @@ struct Fraction {
     auto& operator *= (const Fraction& other) { return *this = (*this * other); }
     auto& operator /= (const Fraction& other) { return *this = (*this / other); }
 
-    auto& operator ++ (const Fraction& other) { return *this += { +1, 1 }; }
-    auto& operator -- (const Fraction& other) { return *this += { -1, 1 }; }
+    auto& operator ++ () { return *this += { +1, 1 }; }
+    auto& operator -- () { return *this += { -1, 1 }; }
 
     friend auto& operator << (std::ostream& os, const Fraction& f) {
         if (f.d == 0)
@@ -111,10 +118,10 @@ struct Fraction {
     }
 #if !FRACTION_NO_STRING_PARSING
     friend auto& operator >> (std::istream& is, Fraction& f) {
-        return f = FractionParser::parse(is), is;
+        return f = /*FractionParser::*/parse(is), is;
     }
     static Fraction fromStr (const std::string s) {
-        return FractionParser::parse(s);
+        return /*FractionParser::*/parse(s);
     }
 #endif // !FRACTION_NO_STRING_PARSING
     
@@ -168,20 +175,70 @@ struct Fraction {
     }
 
 #if !FRACTION_NO_UNITTESTS
-    static void unittest () { 
-        assert(Fraction{ 4, 0 } == Fraction{ 9, 0 });
-        assert(Fraction{ 0, 4 } == Fraction{ 0, 9 });
+    template <typename A, typename B>
+    static void assertEq (const A& a, const B& b) {
+        if (a != b)
+            std::cerr << "Assertion Failure: " << a << " != " << b << '\n';
+    }
+    template <typename A, typename B>
+    static void assertNe (const A& a, const B& b) {
+        if (a == b)
+            std::cerr << "Assertion Failure: " << a << " == " << b << '\n';
+    }
+    template <typename Exception, typename F>
+    static auto assertThrows (const F& f, bool shouldThrow) {
+        try {
+            f();
+            if (shouldThrow)
+                std::cerr << "Assertion Failure: expected thrown exception " << '\n';
+        } catch (const Exception e) {
+            if (!shouldThrow)
+                std::cerr << "Assertion Failure: expected no exception, got " << e->what() << '\n';
+        }
+    }
 
-        assert(Fraction{ 1, 1 } == Fraction { 1, 1 });
-        assert(Fraction{ -1, 1 } != Fraction{ 1, 1 });
-        assert(Fraction{ 8, 4 } == Fraction{ 2, 1 });
+
+    template <typename E, typename R>
+    static R assertThrows (const R& result) {
+
+    }
+
+    #define RUN_TEST(test) std::cout << "\n\nRunning Test: " #test "\n"; test()
+
+    static void unittest () {
+        RUN_TEST(unittest_enforce);
+        RUN_TEST(unittest_skipSpace);
+        RUN_TEST(unittest_parseNumber);
+        RUN_TEST(unittest_resolveExpr);
+        RUN_TEST(unittest_flushOps);
+        RUN_TEST(unittest_parseExpr);
+
+        assertEq(Fraction{ 4, 0 }, Fraction{ 9, 0 });
+        assertEq(Fraction{ 0, 4 }, Fraction{ 0, 9 });
+
+        assertEq(Fraction{ 1, 1 }, Fraction { 1, 1 });
+        assertNe(Fraction{ -1, 1 }, Fraction{ 1, 1 });
+        assertEq(Fraction{ 8, 4 }, Fraction{ 2, 1 });
         
-        assert(Fraction{ 1, 1 } + Fraction { 1, 2 } == Fraction { 3, 2 });
+        assertEq(Fraction{ 1, 1 } + Fraction { 1, 2 }, Fraction { 3, 2 });
 
-        assert({Fraction{ 4, 6 }.str() == "2 / 3");
+        assertEq(Fraction{ 4, 6 }.n, 2);
+        assertEq(Fraction{ 4, 6 }.d, 3);
+        assertEq(Fraction{ 4, 6 }.str(), "2/3");
+        assertEq(Fraction("2/3").n, Fraction(2,3));
+        
+        assertEq(Fraction(1,0).str(), "+inf");
+        assertEq(Fraction(-1,0).str(), "-inf");
+        assertEq(Fraction(0,0).str(), "+inf");
+        assertEq(Fraction("+inf").str(),"+inf");
+        assertEq(Fraction("-inf").str(),"-inf");
+        assertEq(Fraction("inf").str(),"+inf");
+        assertEq(Fraction("+inf"),Fraction(22,0));
+        assertNe(Fraction(-22,0),Fraction(22,0));
+        assertEq(Fraction(12312,0),Fraction(123,0));
     }    
 #endif // !FRACTION_NO_UNITTESTS
-};
+// };
 
 #if !FRACTION_NO_STRING_PARSING
 
@@ -199,17 +256,18 @@ struct Fraction {
 //    this parser is built for simplicity + use of standard library data structures, NOT speed.
 //    No, we wouldn't use this in real-world scenarios.
 //
-struct FractionParser {
-    static void parse (std::istream& is) {
+// struct FractionParser {
+    static Fraction parse (std::istream& is) {
         std::stringstream ss { '(' };
-        is >> ss;
+        ss << is.rdbuf();
         ss << ')';
         return parseExpr(ss);
     }
-    static void parse (std::string& s) {
-        std::stringstream ss { '(' };
-        ss << s;
-        ss << ')';
+    static Fraction parse (const std::string& s) {
+        std::stringstream ss; assert(ss.good());
+        ss << '(';
+        ss << s; assert(ss.good());
+        ss << ')' << std::endl; assert(ss.good());
         return parseExpr(ss);
     }
 
@@ -219,29 +277,58 @@ struct FractionParser {
 
     #define DECL_EXCEPTION(Cls,what) \
     struct Cls : public ParseException { \
-        Cls () : std::ParseException(what) {} \
+        Cls () : ParseException(what) {} \
     };
     DECL_EXCEPTION(ExpectedNumber,   "expected number")
     DECL_EXCEPTION(ExpectedExpr,     "expected expression")
     DECL_EXCEPTION(UnbalancedRParen, "unbalanced ')'")
     DECL_EXCEPTION(UnbalancedExpr,   "unbalanced expr")
+    DECL_EXCEPTION(InvalidOp,        "invalid op")
     #undef DECL_EXCEPTION
 
 protected:
-    template <typename Ex>
-    static void expect (bool condition) {
+    template <typename Ex, typename T>
+    static void enforce (const T& condition) {
         if (!condition)
             throw new Ex();
     }
+    static void unittest_enforce () {
+        assertThrows<ExpectedNumber*>([](){ enforce<ExpectedNumber>(false); }, true);
+        assertThrows<ExpectedNumber*>([](){ enforce<ExpectedNumber>(true); }, false);
+    }
 
     template <typename Ex>
-    static void skipSpace (std::istream& is, int& c) {
+    static void skipSpace (std::istream& is, char& c) {
         while (isspace(c))
-            enforce<Ex>(is.get(c));
+            enforce<Ex>((bool)is.get(c));
+    }
+    static void unittest_skipSpace () {
+        std::stringstream ss { "f  a\t w\tl\nab  \n\tl "};
+        char c = ss.peek();
+        assertEq((char)ss.peek(), 'f'); skipSpace<ExpectedExpr>(ss, c);
+        assertEq((char)ss.peek(), 'f'); ss.get(c);
+        assertEq((char)ss.peek(), ' '); skipSpace<ExpectedExpr>(ss, c);
+        assertEq((char)ss.peek(), 'a'); skipSpace<ExpectedExpr>(ss, c);
+        assertEq((char)ss.peek(), 'a'); ss.get(c);
+        assertEq((char)ss.peek(), '\t'); skipSpace<ExpectedExpr>(ss, c);
+        assertEq((char)ss.peek(), 'w'); ss.get(c);
+        assertEq((char)ss.peek(), '\t'); skipSpace<ExpectedExpr>(ss, c);
+        assertEq((char)ss.peek(), 'l'); ss.get(c);
+        assertEq((char)ss.peek(), '\n'); skipSpace<ExpectedExpr>(ss, c);
+        assertEq((char)ss.peek(), 'a'); skipSpace<ExpectedExpr>(ss, c);
+        assertEq((char)ss.peek(), 'a'); ss.get(c);
+        assertEq((char)ss.peek(), 'b'); skipSpace<ExpectedExpr>(ss, c);
+        assertEq((char)ss.peek(), 'b'); ss.get(c);
+        assertEq((char)ss.peek(), ' '); skipSpace<ExpectedExpr>(ss, c);
+        assertEq((char)ss.peek(), 'l'); skipSpace<ExpectedExpr>(ss, c);
+        assertEq((char)ss.peek(), 'l'); ss.get(c);
+        assertEq((char)ss.peek(), ' '); skipSpace<ExpectedExpr>(ss, c);
+        assertEq((char)ss.peek(), '\0'); skipSpace<ExpectedExpr>(ss, c);
+        assertEq((char)ss.peek(), '\0');
     }
 
     static Fraction parseNumber (std::istream& is) {
-        int c;
+        char c;
 
         enforce<ExpectedNumber>(is && is.get(c));            
         skipSpace<ExpectedNumber>(is, c);
@@ -249,9 +336,9 @@ protected:
         // Handle optional sign
         int sign = 1;
         if (c == '+') {
-            enforce<ExpectedNumber>(is.get(c));
+            enforce<ExpectedNumber>((bool)is.get(c));
         } else if (c == '-') {
-            enforce<ExpectedNumber>(is.get(c));
+            enforce<ExpectedNumber>((bool)is.get(c));
             sign = -1;
         }
 
@@ -266,13 +353,28 @@ protected:
 
         int n = c;
         while (is.get(c) && isdigit(c)) {
-            n = n * 10 + c;
+            n *= 10;
+            n += c;
         }
         if (!isdigit(c))
-            is.unget(c);
+            is.unget();
 
         return Fraction { sign * n, 1 };
     }
+    static void unittest_parseNumber () {
+        std::stringstream ss { "1 +2 -3 123 456 78912301 inf -inf +inf -120941421241241 "}; char c;
+        assertEq(parseNumber(ss).str(), "1"); assert(ss.get(c) && c == ' ');
+        assertEq(parseNumber(ss).str(), "2"); assert(ss.get(c) && c == ' ');
+        assertEq(parseNumber(ss).str(), "-3"); assert(ss.get(c) && c == ' ');
+        assertEq(parseNumber(ss).str(), "123"); assert(ss.get(c) && c == ' ');
+        assertEq(parseNumber(ss).str(), "456"); assert(ss.get(c) && c == ' ');
+        assertEq(parseNumber(ss).str(), "78912301"); assert(ss.get(c) && c == ' ');
+        assertEq(parseNumber(ss).str(), "+inf"); assert(ss.get(c) && c == ' ');
+        assertEq(parseNumber(ss).str(), "-inf"); assert(ss.get(c) && c == ' ');
+        assertEq(parseNumber(ss).str(), "+inf"); assert(ss.get(c) && c == ' ');
+        assertEq(parseNumber(ss).str(), "-120941421241241"); assert(ss.get(c) && c == ' ');
+    }
+
     static Fraction parseExpr (std::istream& is) {
 
         std::vector<Fraction>   terms;
@@ -281,8 +383,10 @@ protected:
         #define GET_OP(v)           (v & 0xff)
         #define GET_PRED(v)         (v >> 16)
         #define MAKE_OP(op, pred)   (op | (pred << 16))
+        
+        assert(is.good());
 
-        int c;
+        char c;
         while (1) {
             enforce<ExpectedExpr>(is.get(c));
             skipSpace<ExpectedExpr>(is, c);
@@ -290,7 +394,7 @@ protected:
             switch (c) {
                 case '(': ops.push_back(MAKE_OP('(', 1)); break;
                 case ')': 
-                    while (enforce<UnbalancedRParen>(ops.size() != 0), ops.back() != '(') {
+                    while (enforce<UnbalancedRParen>(ops.size() != 0), GET_OP(ops.back()) != '(') {
                         resolveExpr(terms, ops);
                     }
                     ops.pop_back();
@@ -304,14 +408,17 @@ protected:
                 case '*': flushOps(terms, ops, '*', 1); break;
                 case '/': flushOps(terms, ops, '/', 1); break;
                 default:
-                    unget(c);
+                    is.unget();
                     parseNumber(is);
             }
         }
     }
+    static void unittest_parseExpr () {
+        // TBD
+    }
+
     static void resolveExpr (std::vector<Fraction>& terms, std::vector<int>& ops) {
-        if (GET_OP(ops.back()) == '(')
-            return;
+        assert(GET_OP(ops.back()) != '(');
 
         enforce<ExpectedExpr>(terms.size() >= 2);
         auto rhs = terms.back(); terms.pop_back();
@@ -326,12 +433,27 @@ protected:
             default: enforce<InvalidOp>(false);
         }
     }
+    static void unittest_resolveExpr () {
+        // TBD
+    }
+
     static void flushOps (std::vector<Fraction>& terms, std::vector<int>& ops, int op, int pred) {
-        while (ops.size() && GET_PRED(ops.back()) <= pred) {
+        while (ops.size() && GET_PRED(ops.back()) <= pred && GET_OP(ops.back()) != '(') {
             resolveExpr(terms, ops);
         }
         ops.push_back(MAKE_OP(op, pred));
     }
+    static void unittest_flushOps () {
+        // TBD
+    }
 };
+
+Fraction::Fraction (const std::string& s) : n(0), d(0) {
+    try {
+        *this = Fraction::parse(s);
+    } catch (Fraction::ParseException* e) {
+        std::cerr << "PARSE ERROR: " << e->what() << " in '" << s << "'\n";
+    }
+}
 
 #endif // !FRACTION_NO_STRING_PARSING
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+
+#include "fraction.hpp"
+
+int main (int argc, const char** argv) {
+    Fraction::unittest();
+}
